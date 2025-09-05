26027843d9c799a360b4c4eeffd6c682833a891e
smc7002 <smc6189@gmail.com>
2025-09-04 17:08:25 -0700

    Stop tracking network monitor .bat file

--- 네트워크 모니터.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ====================================
-echo    TraceNet ???? ??? ??
-echo ====================================
-cd /d "%~dp0\server"
-echo ?? ?? ?... (3? ? ???? ?? ??)
-start "" "http://localhost:5000"
-timeout /t 3 /nobreak >nul
-dotnet run
